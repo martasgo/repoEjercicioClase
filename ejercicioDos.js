65e60f7c7c5442244962df9af89fe62d7459f9f6
emanuelMoralez <emanuel.moralez@gmail.com>
2023-10-04 21:59:28 -0300

    prueba de solucion

--- ejercicioDos.js
+++ ejercicioDos.js
@@ -1,1 +1,10 @@
 console.log ("Hola Santiago");
+
+let suma = (numero1, numero2) => numero1 + numero2;
+let resta = (numero1, numero2) => numero1 - numero2;
+let multiplicacion = (numero1, numero2) => numero1 * numero2;
+let division = (numero1, numero2) => numero1 / numero2;
+
+let calculadora = (numero1, numero2, callback) => {
+  return callback (suma, resta, multiplicacion, division);
+};
